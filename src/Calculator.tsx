776c6209be1705499b4828a0308c8527de71d449
Colby Jensen <endergdawg@gmail.com>
2024-07-23 09:50:17 -0600

    Add styling

--- src/Calculator.tsx
+++ src/Calculator.tsx
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
-
+import styles from "./Calculator.module.css";
 
 
 const Calculator: React.FC = () => {
     const [name, setName] = useState("");
     const [age, setAge] = useState<number | string>("");
-    const [ageInDays, setAgeInDays] = useState(0)
+    const [ageInDays, setAgeInDays] = useState<number | null>(null);
 
     // Takes in the text box contents
     const handleAgeChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
@@ -22,9 +22,9 @@ const Calculator: React.FC = () => {
 
 
     return (
-        <>
-        <form onSubmit={calcAge}>
-            <div id="calculator">
+        <div className={styles.calculator}>
+        <form onSubmit={calcAge} className={styles.calculatorForm}>
+            <div id="calculator" className={styles.formGroup} >
                 <label htmlFor="name">Name</label>
                 <input 
                 type="text" 
@@ -40,11 +40,12 @@ const Calculator: React.FC = () => {
                 value={age}
                 placeholder="1000 y/o"
                 onChange={handleAgeChange}/>
-                <input type="submit"/>
-                <h3>Congrats {name}! You are {ageInDays} days old!</h3>
+                <button type="submit">Submit Me</button>
             </div>
         </form>
-        </>
+        {/* If statement, will not show results until ageInDays is not null */}
+        {ageInDays && <h3>Congrats {name ? name : "person"}! You are {ageInDays} days old!</h3>}
+        </div>
     )
 }
 
